a8e65dc85c8c0caee30c905c40d9fb90f5ca33d3
Elg <elg@mail>
2019-08-01 13:04:35 +0200

    Force flushing of std out buffer

--- Primitive.hs
+++ Primitive.hs
@@ -1,9 +1,10 @@
 module Primitive where
 
-import           Control.Monad
-import           Syntax
+import Control.Monad
+import Syntax
+import System.IO
 
-primitives :: [(String, Value)]
+primitives :: Env
 primitives =
   [ ("__u-", VPrimFun $ \[VInt x] -> VInt $ -x)
   , ("__u!", VPrimFun $ \[VBool x] -> VBool $ not x)
@@ -34,7 +35,7 @@ primitives =
   , ("__b&&", VPrimFun $ \[VBool x, VBool y] -> VBool $ x && y)
   , ("__b||", VPrimFun $ \[VBool x, VBool y] -> VBool $ x || y)
   , ("readln", VPrimFunIO $ \[] -> getLine >>= \inp -> return $ VString inp)
-  , ("print", VPrimFunIO $ \args -> mapM_ (putStr . show) args >> return VVoid)
+  , ("print", VPrimFunIO $ \args -> mapM_ (putStr . show) args >> hFlush stdout >> return VVoid)
   , ( "println"
     , VPrimFunIO $ \args ->
         mapM_ (putStr . show) args >> putStrLn "" >> return VVoid)
