20d22cf45a5bd8296e02c6ca5b75670b7f17a32f
Elg <elg@mail>
2019-08-07 11:15:58 +0200

    Rework primitive equality and array functions
    * Equality uses value equality
    * Array primitives no longer auto unboxes values
    * Add toString function that WILL auto unbox references in arrays

--- Primitive.hs
+++ Primitive.hs
@@ -2,7 +2,9 @@ module Primitive where
 
 import Control.Monad
 import Syntax
+import Data.IORef
 import System.IO
+import Data.Maybe
 
 primitives :: Env
 primitives =
@@ -15,20 +17,14 @@ primitives =
   , ("__b*", VPrimFun "*" $ \[VInt x, VInt y] -> VInt $ x * y)
   , ("__b/", VPrimFun "/" $ \[VInt x, VInt y] -> VInt $ x `div` y)
   , ("__b%", VPrimFun "%" $ \[VInt x, VInt y] -> VInt $ x `mod` y)
-  , ( "__b=="
-    , VPrimFun "==" $ \args ->
-        case args of
-          [VBool x, VBool y] -> VBool $ x == y
-          [VInt x, VInt y]   -> VBool $ x == y
-          [VString x, VString y]   -> VBool $ x == y
-          [v1, v2] -> error $ "No way to compare "++val2type v1 ++" to "++val2type v2)
-  , ( "__b!="
-    , VPrimFun "!=" $ \args ->
-        case args of
-          [VBool x, VBool y] -> VBool $ not $ x == y
-          [VInt x, VInt y]   -> VBool $ not $ x == y
-          [VString x, VString y] -> VBool $ not $ x == y
-          [v1, v2] -> error $ "No way to compare "++val2type v1 ++" to "++val2type v2)
+  , ("__b==", VPrimFun "==" $ \args ->
+          case args of 
+            [VArr es1, VArr es2] -> VBool $ (VArr (readRefs es1)) == (VArr (readRefs es2))
+            [e1, e2] -> VBool $ e1 == e2)
+  , ("__b!=", VPrimFun "!=" $ \args ->
+          case args of 
+            [VArr es1, VArr es2] -> VBool $ (VArr (readRefs es1)) /= (VArr (readRefs es2))
+            [e1, e2] -> VBool $ e1 /= e2)
   , ("__b<", VPrimFun "<" $ \[VInt x, VInt y] -> VBool $ x < y)
   , ("__b<=", VPrimFun "<=" $ \[VInt x, VInt y] -> VBool $ x <= y)
   , ("__b>", VPrimFun ">" $ \[VInt x, VInt y] -> VBool $ x > y)
@@ -36,23 +32,45 @@ primitives =
   , ("__b&&", VPrimFun "&&"$ \[VBool x, VBool y] -> VBool $ x && y)
   , ("__b||", VPrimFun "||"$ \[VBool x, VBool y] -> VBool $ x || y)
   -- array functions
-  , ("length", VPrimFun "length" $ \[VArr xs] -> VInt $ length xs) -- the length of an array
-  , ("add", VPrimFun "add"$ \[VArr xs, e] -> VArr $ xs ++ [EVal e]) -- return a new array with the added eent
-  , ("insert", VPrimFun "insert" $ \[VArr xs, e, VInt index] -> do -- insert an element at the given index
-                  let (a,b) = splitAt index xs
-                  VArr $ a ++ (EVal e : b))
-  , ("remove", VPrimFun "remove"$ \[VArr xs, VInt index] ->  
-                  if index <= 0 then  VArr $ if null xs then [] else tail xs
-                  else do
-                    let (a,b) = splitAt (index+1) xs
-                    VArr $ safeInit a ++ b)
-  , ("create", VPrimFun "create" $ \[VInt size] -> VArr $ replicate size $ ERef $ EVal VVoid) -- create an array with the given size and void as eent
-  -- real IO funcs
+  , ("length", VPrimFunIO "length" $ \args -> -- the length of an array
+      case args of 
+        [VRef rh _] -> do -- allow for reference arrays to 
+          (VArr xs) <- readIORef rh
+          return $ VInt $ length xs
+        [VArr xs] -> return $ VInt $ length xs)
+  , ("add", VPrimFunIO "add"$ \args -> -- return a new array with the added element
+      case args of 
+        [VRef rh _, e] -> do
+          VArr xs <- readIORef rh
+          return $ VArr $ xs ++ [EVal e]) 
+  , ("insert", VPrimFunIO "insert" $ \args -> -- insert an element at the given index
+      case args of  
+        [VRef rh _, e, VInt index] -> do
+          VArr xs <- readIORef rh
+          let (a,b) = splitAt index xs
+          return $ VArr $ a ++ (EVal e : b))                  
+  , ("remove", VPrimFunIO "remove"$ \args ->
+            case args of 
+              [VRef rh _, VInt index] -> do
+                VArr xs <- readIORef rh
+                if index <= 0 then return $ VArr $ if null xs then [] else tail xs
+                else do
+                  let (a,b) = splitAt (index+1) xs
+                  return $ VArr $ safeInit a ++ b)
+  , ("create", VPrimFun "create" $ \[VInt size] -> VArr $ replicate size $ ERef $ EVal VVoid) -- create an array with the given size and void as elemt
+  , ("toString", 
+      VPrimFunIO "toString" $ \args -> -- print out arrays as a string (no spaces between elements)
+        case args of 
+            [VRef rh _] -> do
+              val <- readIORef rh
+              let (VPrimFunIO _ f) = fromJust (lookup "toString" primitives)
+              f [val]
+            [VArr xs] -> return $ VString $ concatMap (\ev -> case ev of{EVal v -> show v;_ -> show ev}) (readRefs xs)
+            [v] -> return $ VString $ show v)
+            
   , ("readln", VPrimFunIO "readln" $ \[] -> getLine >>= \inp -> return $ VString inp)
   , ("print", VPrimFunIO "print" $ \args -> mapM_ (putStr . show) args >> hFlush stdout >> return VVoid)
-  , ( "println"
-    , VPrimFunIO "println" $ \args ->
-        mapM_ (putStr . show) args >> putStrLn "" >> return VVoid)
+  , ("println" , VPrimFunIO "println" $ \args -> mapM_ (putStr . show) args >> putStrLn "" >> return VVoid)
   ]
 
 safeInit :: [a] -> [a]
